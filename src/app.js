5cf64719f000a3b0ef21aa1619d446346470c4f3
Amar Bhamra <amarbham@hotmail.com>
2020-07-10 23:06:16 +0100

    remove debug code

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ const param = yargs.argv.param;
 
 process.stdout.write(`Please wait... \n executing ${param} \n`);
 
-webScraper.init(endoDriverUrls);
-
 if (param === 'webscraper') {
     webScraper.init(endoDriverUrls);
 }
